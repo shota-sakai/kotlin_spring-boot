b97050203687b2da2f89dd7da84930ddfd678126
shota-sakai <atoysiakas@gmail.com>
2024-03-02 05:42:17 +0000

    確認用ソースの追加

--- customer-api/src/main/kotlin/com/example/demo/utils/EventBridgeUtils.kt
+++ customer-api/src/main/kotlin/com/example/demo/utils/EventBridgeUtils.kt
@@ -4,14 +4,15 @@ import kotlinx.coroutines.runBlocking
 import aws.sdk.kotlin.services.eventbridge.model.PutEventsRequestEntry
 import aws.sdk.kotlin.services.eventbridge.model.PutEventsRequest
 import aws.sdk.kotlin.services.eventbridge.EventBridgeClient
+import java.time.OffsetTime
 
 class EventBridgeUtils(private val value: String) {
   fun triggerCustomRule() = runBlocking {
     val entry = PutEventsRequestEntry {
         source = "test"
-        detail = "{}"
-        // detail = "{\"c-count\": \"test\"}"
+        detail = "{\"hogehoge\": \"foofoo\"}"
         detailType = "testDetail"
+        eventBusName = "TestEventBus"
     }
 
     val eventsRequest = PutEventsRequest {
@@ -20,7 +21,7 @@ class EventBridgeUtils(private val value: String) {
 
     EventBridgeClient { region = value }.use { eventBrClient ->
         val response = eventBrClient.putEvents(eventsRequest)
-        println("結果=$response")
+        println("結果=$response, 日時=${OffsetTime.now()}")
     }    
   }
 }
